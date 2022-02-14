b572f53f0c64e181cfcebf91cbfbbf9547c50de9
Danila Kamaev <danila@kamaeff.ru>
2022-02-14 22:33:08 +0300

    Добавил контест-процессор year

---
+++ yatube/core/context_processors/year.py
@@ -0,0 +1,10 @@
+"""Кастомные контекст-процессоры для шаблонов."""
+
+import datetime as dt
+
+
+def year(request):
+    """Добавляет переменную с текущим годом."""
+    return {
+        'year': dt.date.today().year
+    }
